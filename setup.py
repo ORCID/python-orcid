f01a94c841ac03d79440744728e22879720fc658
Mateusz Susik <mateusz.susik@cern.ch>
2014-12-16 13:07:46 +0100

    setup file
    Signed-off-by: Mateusz Susik <mateusz.susik@cern.ch>

---
+++ setup.py
@@ -0,0 +1,15 @@
+"""Setup file for python-orcid."""
+
+from distutils.core import setup
+setup(name='python-orcid',
+      packages=['python-orcid'],
+      version='0.0',
+      description='A python wrapper over ORCID API',
+      author='Mateusz Susik',
+      author_email='mateuszsusik@gmail.com',
+      url='https://github.com/MSusik/python-orcid',
+      # TO DO
+      # download_url='https://github.com/.../tarball/0.1',
+      keywords=['orcid', 'api', 'wrapper'],
+      classifiers=[],
+      )
