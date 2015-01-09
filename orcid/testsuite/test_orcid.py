2b2a17491d2c377dbc419ba1130394e303d5c140
Mateusz Susik <mateusz.susik@cern.ch>
2015-01-09 17:29:03 +0100

    travis routine
    Signed-off-by: Mateusz Susik <mateusz.susik@cern.ch>

---
+++ orcid/testsuite/test_orcid.py
@@ -0,0 +1,6 @@
+"""General tests for ORCID library."""
+
+
+def test_dummy():
+    """A dummy test."""
+    assert 1 == 1
--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 """Setup file for python-orcid."""
 
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(self.test_args)
+        raise SystemExit(errno)
+
 setup(author='Mateusz Susik',
       author_email='mateuszsusik@gmail.com',
       classifiers=[],
+      cmdclass={'test': PyTest},
       description='A python wrapper over ORCID API',
       install_requires=['jinja2', 'lxml', 'requests'],
       keywords=['orcid', 'api', 'wrapper'],
       name='orcid',
       package_data={'orcid': ['templates/*.xml']},
       packages=['orcid'],
+      tests_require=['pytest'],
       url='https://github.com/MSusik/python-orcid',
       version='0.0'
       # TO DO
