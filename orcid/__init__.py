3750807f22434b3a2dd9bf0e1698502a055c3b2c
Mateusz Susik <mateusz.susik@cern.ch>
2015-04-05 22:53:21 +0200

    general: bump to API version 2.0_rc1
    Signed-off-by: Mateusz Susik <mateusz.susik@cern.ch>

--- orcid/__init__.py
+++ orcid/__init__.py
@@ -1,8 +1,8 @@
 """Initialization file for python-orcid."""
 
 from .orcid import add_external_id
-from .orcid import get_id, get_info, push_data
+from .orcid import get_info, push_data
 from .orcid import set_credentials, set_endpoint
 
-__all__ = ('add_external_id', 'get_id', 'get_info', 'push_data',
+__all__ = ('add_external_id', 'get_info', 'push_data',
            'set_credentials', 'set_endpoint')
--- orcid/orcid.py
+++ orcid/orcid.py
@@ -16,16 +16,16 @@ class Config:
 Config.SANDBOX = False
 
 Config.ENDPOINT_PUBLIC = "http://pub.orcid.org"
-Config.ENDPOINT_MEMBER = "https://api.orcid.org/"
-
-Config.KEY = None
-Config.SECRET = None
+Config.ENDPOINT_MEMBER = "https://api.orcid.org"
 
 Config.MEMBER_KEY = None
 Config.MEMBER_SECRET = None
-
 Config.SANDBOX_KEY = None
 Config.SANDBOX_SECRET = None
+Config.KEY = None
+Config.SECRET = None
+
+VERSION = "/v2.0_rc1"
 
 
 def set_endpoint(sandbox=False):
@@ -34,17 +34,20 @@ def set_endpoint(sandbox=False):
     Change key and secret to appropiate ones. This function is useful if you
     want to test your code using sandbox.
 
-    :param sandbox: if yes, sandbox is used, if no, production endpoint is used
-    :type sandbox: bool
+    Parameters
+    ----------
+    :param sandbox: boolean
+        If true, sandbox is used. Otherwise, production endpoint is used.
     """
     Config.SANDBOX = sandbox
 
     if sandbox:
         Config.ENDPOINT_MEMBER = "http://api.sandbox.orcid.org"
+        Config.ENDPOINT_PUBLIC = "http://pub.sandbox.orcid.org"
         Config.KEY = Config.SANDBOX_KEY
         Config.SECRET = Config.SANDBOX_SECRET
     else:
-        Config.ENDPOINT_MEMBER = "https://api.orcid.org/"
+        Config.ENDPOINT_MEMBER = "https://api.orcid.org"
         Config.ENDPOINT_PUBLIC = "http://pub.orcid.org"
         Config.KEY = Config.MEMBER_KEY
         Config.SECRET = Config.MEMBER_SECRET
@@ -55,12 +58,14 @@ def set_credentials(key, secret, sandbox=False):
 
     You can specify sandbox parameter if you want to set sandox credentials.
 
-    :param key: the institution's key
-    :type key: string
-    :param secret: the institution's secret
-    :type secret: string
-    :param sandbox: parameter indicating which credentials should be set
-    :type sandbox: bool
+    Parameters
+    ----------
+    :param key: string
+        The institution's key
+    :param secret: string
+        The institution's secret
+    :param sandbox: bool
+        If true, sandbox is used. Otherwise, production endpoint is used.
     """
     if sandbox:
         Config.SANDBOX_KEY = key
@@ -98,40 +103,56 @@ def _get_access_token_from_orcid(scope):
 # GETTING INFORMATION #########################################################
 
 
-def _get_member_info(orcid_id, request_type, response_format):
+def _get_member_info(orcid_id, request_type, response_format, id):
     access_token = _get_access_token_from_orcid('/read-public')
-    request_url = '%s/%s/%s' % (Config.ENDPOINT_MEMBER,
+    request_url = '%s/%s/%s' % (Config.ENDPOINT_MEMBER + VERSION,
                                 orcid_id, request_type)
+    if id:
+        request_url += '/%s' % id
     headers = {'Accept': 'application/orcid+%s' % response_format,
                'Authorization': 'Bearer %s' % access_token}
     return requests.get(request_url, headers=headers)
 
 
-def _get_public_info(orcid_id, request_type, response_format):
+def _get_public_info(orcid_id, request_type, response_format, id):
 
-    if Config.SANDBOX:
-        # unfortunately, there is no public API for sandbox
-        return _get_member_info(orcid_id, request_type, response_format)
-
-    request_url = '%s/%s/%s' % (Config.ENDPOINT_PUBLIC,
+    request_url = '%s/%s/%s' % (Config.ENDPOINT_PUBLIC + VERSION,
                                 orcid_id, request_type)
+    if id:
+        request_url += '/%s' % id
     headers = {'Accept': 'application/orcid+%s' % response_format}
     return requests.get(request_url, headers=headers)
 
 
-def get_info(orcid_id, scope, request_type, response_format='json'):
+def get_info(orcid_id, scope, request_type, response_format='json',
+             id=None):
     """Get the researcher's ORCID.
 
-    :request_type: one of orcid-bio, orcid-profile, orcid-works
-    :response_format: one of json, xml.
-    :scope: public or member
+    Parameters
+    ----------
+    :param orcid_id: string
+        Id of the queried author
+    :param scope: string
+        'public' or 'member'
+    :param request_type: string
+        One of 'activities', 'education', 'employment', 'work', 'funding'
+    :param response_format: one of json, xml.
     """
+    if request_type != "activities" and not id:
+        raise ValueError("""In order to fetch specific record, please specify
+                            the 'id' argument.""")
+    elif request_type == "activities" and id:
+        raise ValueError("""In order to fetch activities summary, the 'id'
+                            argument is redundant.""")
+
     if scope == "member":
-        response = _get_member_info(orcid_id, request_type, response_format)
+        response = _get_member_info(orcid_id, request_type, response_format,
+                                    id)
     elif scope == "public":
-        response = _get_public_info(orcid_id, request_type, response_format)
+        response = _get_public_info(orcid_id, request_type, response_format,
+                                    id)
     else:
-        raise ValueError
+        raise ValueError("""Wrong scope.""")
 
     code = response.status_code
 
@@ -141,11 +162,6 @@ def get_info(orcid_id, scope, request_type, response_format='json'):
         return json.loads(response.content)
     elif response_format == "xml":
         return etree.fromstring(response.content)
-
-
-def get_id(orcid_id, scope, response_format='json'):
-    """Get the researcher's biography from ORCID."""
-    return get_info(orcid_id, scope, 'orcid-bio', response_format)
 
 # UPDATING DATA ###############################################################
 
@@ -215,3 +231,7 @@ def push_data(orcid_id, scope, token, formatted_data, render=True,
     except requests.exceptions.HTTPError, exc:
         print exc
         print response.text
+
+# INITIALIZATION ##############################################################
+
+set_endpoint()
