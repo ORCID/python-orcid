d6832c4c957e084abac7e148d2f90e8e61165db5
Mateusz Susik <mateusz.susik@cern.ch>
2015-04-17 13:22:43 +0200

    tests: first test for coverage
    Signed-off-by: Mateusz Susik <mateusz.susik@cern.ch>

--- orcid/testsuite/test_orcid.py
+++ orcid/testsuite/test_orcid.py
@@ -1,6 +1,10 @@
 """General tests for ORCID library."""
 
+from orcid import MemberAPI, PublicAPI
 
-def test_dummy():
-    """A dummy test."""
-    assert 1 == 1
+
+def test_search_public():
+    """Test search_public."""
+    api = PublicAPI(sandbox=True)
+    results = api.search_public('family-name:Sanchez')
+    assert results != 1
